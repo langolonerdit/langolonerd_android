4bdee5d788d8ba6b85673082f679928343f0e13a
Aronne Brivio <aroblu94@gmail.com>
2017-01-08 19:59:50 +0100

    Check and fix auto-gen part

--- app/src/main/java/it/langolonerd/app/MainActivity.java
+++ app/src/main/java/it/langolonerd/app/MainActivity.java
@@ -1,22 +1,3 @@
-/*
-       Licensed to the Apache Software Foundation (ASF) under one
-       or more contributor license agreements.  See the NOTICE file
-       distributed with this work for additional information
-       regarding copyright ownership.  The ASF licenses this file
-       to you under the Apache License, Version 2.0 (the
-       "License"); you may not use this file except in compliance
-       with the License.  You may obtain a copy of the License at
-
-         http://www.apache.org/licenses/LICENSE-2.0
-
-       Unless required by applicable law or agreed to in writing,
-       software distributed under the License is distributed on an
-       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-       KIND, either express or implied.  See the License for the
-       specific language governing permissions and limitations
-       under the License.
- */
-
 package it.langolonerd.app;
 
 import android.app.Activity;
@@ -129,8 +110,7 @@ public class MainActivity extends Activity {
      */
     public Action getIndexApiAction() {
         Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
+                .setName("L'angolo nerd")
                 .setUrl(Uri.parse("http://www.langolonerd.it"))
                 .build();
         return new Action.Builder(Action.TYPE_VIEW)
