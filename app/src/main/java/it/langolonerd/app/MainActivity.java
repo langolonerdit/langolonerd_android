64c482880b7d407e4847743c53a92aa33953c3fb
Aronne Brivio <aroblu94@gmail.com>
2017-01-08 21:31:40 +0100

    Added GPL 3.0 License
    Fix #3

--- app/src/main/java/it/langolonerd/app/MainActivity.java
+++ app/src/main/java/it/langolonerd/app/MainActivity.java
@@ -1,3 +1,23 @@
+/*
+ * L'angolo nerd
+ * Copyright (C) 2017 Aronne Brivio
+ *
+ * This file is part of L'angolo nerd Android application.
+ *
+ * OpenPGP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenPGP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with L'angolo nerd.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package it.langolonerd.app;
 
 import android.app.Activity;
